9d77a221b735d0e8e90bb27722416da98f2dcaff
igorkostin <igor.kostin@vegait.rs>
2024-05-15 12:04:26 +0200

    IAM module

---
+++ infrastructure/module/iam/output.tf
@@ -0,0 +1,7 @@
+# output "iam_iam-assumable-role-with-oidc" {
+#   value = module.iam_iam-assumable-role-with-oidc
+# }
+#
+# output "iam_github_oidc_provider" {
+#   value = module.iam_github_oidc_provider
+# }
