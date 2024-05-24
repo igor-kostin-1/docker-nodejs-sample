23d427a3803a7dfeeac9c6995a444218f0d484fa
igorkostin <igor.kostin@vegait.rs>
2024-05-24 12:27:32 +0200

    removed unnecessary var

--- infrastructure/module/eks/variable.tf
+++ infrastructure/module/eks/variable.tf
@@ -4,11 +4,6 @@ variable "node_subnets_id" {
   type = list(string)
 }
 
-variable "control_subnets_id" {
-  description = "Subnets Id used for creation of the EKS cluster"
-  type = list(string)
-}
-
 variable "vpc_id" {
   description = "Subnets Id used for creation of the EKS cluster"
   type = string
