bc82451ef097ff1ee5933f2546dda9e8e72bfd2c
igorkostin <igor.kostin@vegait.rs>
2024-05-30 13:00:30 +0200

    Removed unnecessary resource.

--- infrastructure/module/dns/data.tf
+++ infrastructure/module/dns/data.tf
@@ -13,8 +13,3 @@ data "aws_lb" "app_alb"{
   arn  = "arn:aws:elasticloadbalancing:eu-central-1:162340708442:loadbalancer/app/k8s-vegaittr-app-74aae18182/8920f6d3d2b0d671"
   name = "k8s-vegaittr-app-74aae18182"
 }
-
-data "aws_lb_listener" "selected443" {
-  load_balancer_arn = data.aws_lb.app_alb.arn
-  port              = 443
-}
