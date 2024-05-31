3faa28818872168659554b469a46819afc91b825
igorkostin <igor.kostin@vegait.rs>
2024-05-31 08:52:29 +0200

    Matching alb data resource by tags(more dynamic solution), removed unnecessary acm certificate.

--- infrastructure/module/dns/data.tf
+++ infrastructure/module/dns/data.tf
@@ -1,15 +1,13 @@
-
-data "aws_acm_certificate" "certificate" {
-  domain   = "igor-kostin.omega.devops.sitesstage.com"
-  statuses = ["ISSUED"]
-}
-
 data "aws_route53_zone" "zone" {
-  name         = "omega.devops.sitesstage.com"
+  name         = var.hosted_zone_name
   private_zone = false
 }
 
 data "aws_lb" "app_alb"{
-  arn  = "arn:aws:elasticloadbalancing:eu-central-1:162340708442:loadbalancer/app/k8s-vegaittr-app-74aae18182/8920f6d3d2b0d671"
-  name = "k8s-vegaittr-app-74aae18182"
+  depends_on = [var.created]
+  tags = {
+    "elbv2.k8s.aws/cluster" : "igork-cluster",
+    "ingress.k8s.aws/resource" : "LoadBalancer",
+    "ingress.k8s.aws/stack": "vegait-training/ecr-igork-private"
+  }
 }
---
+++ infrastructure/module/dns/variable.tf
@@ -0,0 +1,8 @@
+variable "hosted_zone_name"{
+  description = "Hosted zone name."
+  type = string
+}
+
+variable "created" {
+  description = "Helm Chart app created."
+}
